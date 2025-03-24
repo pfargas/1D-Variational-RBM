77fef489b1f6430d563b6b0fa1804d846b48f0d8
Pau Fargas <pfargasreixats@gmail.com>
2025-03-24 15:46:18 +0100

    PT working

--- parallel_tempering.jl
+++ parallel_tempering.jl
@@ -1,4 +1,10 @@
 using ProgressMeter
+using Logging
+using Base.Threads
+
+io = open("log.log", "w+")
+logger = SimpleLogger(io)
+global_logger(logger)
 
 include("metropolis.jl")
 
@@ -35,53 +41,62 @@ function parallel_tempering(exchanges, PTParameters::PTParams, MetropolisParams:
     StepSize = MetropolisParams.StepSize
     GaussianStep = MetropolisParams.GaussianStep
 
-    temperatures = range(TMax, TMin, length=NTemps)
-    x = initial_guess
-    total_chains = zeros(Nexchanges,NTemps, NSteps)
+    # temperatures = range(TMin, TMax, length=NTemps)
 
-    @showprogress for step in 1:PTParameters.Nexchanges
+    # TN=T0*λ^N
+
+    temperatures = TMax * (TMin/TMax).^(range(0, NTemps-1, length=NTemps))
+
+    x = initial_guess*ones(NTemps)
+    x = x .+ randn(NTemps)*0.1
+    @info "Temperatures: $temperatures"
+    @info "Initial guess: $x"
+    total_chains = zeros(Nexchanges+1,NTemps, NSteps)
+
+    @showprogress for step in 1:PTParameters.Nexchanges+1
         # Sampling step
         chains = zeros(NTemps, NSteps)
         for i in 1:NTemps # parallel!!!!!!!
             # x, chain = metropolis(NSteps, f, x, StepSize, GaussianStep, args)
-            x, chains[i, :] = metropolis_energy(NSteps, f, x, StepSize, GaussianStep, temperatures[i], args)
-             #chains[1] is the chain of the first temperature, chains[2] is the chain of the second temperature, etc
+            
+            x[i], total_chains[step ,i, :] = metropolis_energy(NSteps, f, x[i], StepSize, GaussianStep, temperatures[i], args)
+            #chains[1] is the chain of the first temperature, chains[2] is the chain of the second temperature, etc
         end
         # Exchange step
         for i in 1:NTemps-1 # starting from highest temperature, going to lowest
             # exchange for temperature i and i-1
-            ΔE_exchange = (f(chains[i, end], args...) - f(chains[i+1, end], args...)) * (1/temperatures[i+1] - 1/temperatures[i])
+            ΔE_exchange = (f(total_chains[step, i, end], args...) - f(total_chains[step, i+1, end], args...)) * (1/temperatures[i] - 1/temperatures[i+1])
             if ΔE_exchange < 0
-                chains[i, :], chains[i+1, :] = chains[i+1, :], chains[i, :]
-                append!(exchanges.Texchange, temperatures[i])
-                append!(exchanges.ExchangeStep, i)
+                x[i], x[i+1] = x[i+1], x[i]
+                # append!(exchanges.Texchange, temperatures[i])
+                # append!(exchanges.ExchangeStep, i)
             else
                 if rand() < exp(-ΔE_exchange)
-                    chains[i, :], chains[i+1, :] = chains[i+1, :], chains[i, :]
-                    append!(exchanges.Texchange, temperatures[i])
-                    append!(exchanges.ExchangeStep, i)
+                    x[i], x[i+1] = x[i+1], x[i]
+                    # append!(exchanges.Texchange, temperatures[i])
+                    # append!(exchanges.ExchangeStep, i)
                 end
             end
         end
-        total_chains[step, :, :] = chains
+        if step == PTParameters.Nexchanges+1
+            for i in 1:NTemps
+                _, total_chains[step, i, :] = metropolis_energy(NSteps, f, x[i], StepSize, GaussianStep, temperatures[i], args)
+            end
+        end
     end
     return total_chains[end,end,end], total_chains, exchanges
 
 end
 
-function flatten_chains(chains)
-    new_chains = zeros(size(chains,1)*size(chains,3),size(chains,2)) # whole chain, temperature
-    for i in 1:size(chains,2)
-        temp_chain = zeros(1)
-        for j in 1:size(chains,1)
-            append!(temp_chain, chains[j,i,:])
-        end
-        new_chains[:,i] = temp_chain
+function flatten_chains(chains, temp_idx)
+    temp_chain = zeros(1)
+    for j in 1:size(chains,1)
+        append!(temp_chain, chains[j,temp_idx,:])
     end
-    return new_chains
+    return temp_chain
 end
 
-gaussian_energy_landscape(x,args...) = -5*exp(-x^2/0.25)-exp(-(x-2.5)^2/0.75)-exp(-(x+1.5)^2/5)+x^2/10
+gaussian_energy_landscape(x,args...) = -5*exp(-x^2)-exp(-2*(x-2.5)^2)-exp(-(x+1.5)^2/5)+x^2/10
 plot_landscapes = true
 using PyPlot
 clf()
@@ -94,13 +109,13 @@ end
 
 #lets find the min with parallel tempering
 
-initial_guess = -15.0
-TMax = 2.0
+initial_guess = -.50
+TMax = 10.0
 TMin = 1.0
-NTemps = 4
-Nexchanges = 100
+NTemps = 100
+Nexchanges = 200
 NSteps = 100
-StepSize = 0.01
+StepSize = 0.0001
 GaussianStep = true
 
 PTParameters = PTParams(TMax, TMin, NTemps, Nexchanges)
@@ -108,15 +123,37 @@ MetropolisParameters = MetropolisParams(NSteps, StepSize, GaussianStep)
 
 exchanges = Exchanges([], [])
 
+quadratic_energy_landscape(x,args...) = x^2
+
 final, chains, _ = parallel_tempering(exchanges, PTParameters, MetropolisParameters, gaussian_energy_landscape, initial_guess)
 
-chains = flatten_chains(chains)
-print(chains) # LOOK INTO FLATTEN CHAINS
+chains1 = flatten_chains(chains, 1)
 
-for i in 1:NTemps
-    plot(chains[:,i,:], label="T = $(round(range(TMax, TMin, length=NTemps)[i], digits=2))")
+
+# for i in 1:NTemps
+#     plot(chains[:,i,:], label="T = $(round(range(TMax, TMin, length=NTemps)[i], digits=2))")
+# end
+
+# plot(chains1, label="T = $(round(range(TMax, TMin, length=NTemps)[1], digits=2))")
+
+# chains2 = flatten_chains(chains, 2)
+# plot(chains2, label="T = $(round(range(TMax, TMin, length=NTemps)[2], digits=2))")
+
+# chains3 = flatten_chains(chains, 3)
+# plot(chains3, label="T = $(round(range(TMax, TMin, length=NTemps)[3], digits=2))")
+
+# lowest_temp_chain = flatten_chains(chains, NTemps)
+# sec_lowest_temp_chain = flatten_chains(chains, NTemps-1)
+# plot(sec_lowest_temp_chain[size(sec_lowest_temp_chain,1)-2*NSteps:end], label="T = $(round(range(TMax, TMin, length=NTemps)[NTemps-1], digits=2))")
+
+# plot(lowest_temp_chain[size(lowest_temp_chain,1)-2*NSteps:end], label="T = $(round(range(TMax, TMin, length=NTemps)[NTemps], digits=2))")
+
+# for i in NTemps-2:NTemps
+for i in 1:5
+    plot(flatten_chains(chains, i), label="T = $(round(range(TMax, TMin, length=NTemps)[i], digits=2))")
 end
 
 title("$final")
 legend()
 savefig("out/chain.png")
+close(io)
